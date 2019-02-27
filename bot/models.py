6ea994d9dc76cded12bf97c038ff3fc7d5139f94
Vakosta <zzoorm@gmail.com>
2019-02-27 14:30:08 +0300

    Add models

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Step(models.Model):
+    title = models.CharField(verbose_name='заголовок',
+                             max_length=100,
+                             null=True)
+    message = models.TextField(verbose_name='сообщение',
+                               max_length=3000,
+                               null=False)
+
+
+class Player(models.Model):
+    name = models.CharField(verbose_name='имя',
+                            max_length=100,
+                            null=True)
+    step = models.ForeignKey(verbose_name='шаг',
+                             to=Step,
+                             on_delete=models.SET_NULL,
+                             null=True)
