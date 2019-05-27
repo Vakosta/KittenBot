d5d6edcd1e93c4f2569d0041a7eda10cbe986250
Vakosta <zzoorm@gmail.com>
2019-05-27 15:24:06 +0300

    Hot fixes

--- KittenBot/settings.py
+++ KittenBot/settings.py
@@ -111,3 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+    # Add to this list all the locations containing your static files
+)
